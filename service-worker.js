92ee4f52398f88798ee27fb86462df575d528d4c
mennykaufman <mennykaufman@gmail.com>
2026-07-27 01:29:36 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ const APP_SHELL = [
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
-  "./icons/splash.png",
+  "./icons/splash.jpg",
 ];
 
 self.addEventListener("install", (event) => {
